d46961e9d6010deb5285bf98ea77d973e506ef24
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-11-19 10:47:05 -0500

    add str func

--- products/models.py
+++ products/models.py
@@ -12,10 +12,15 @@ class Product(models.Model):
 class Package(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=1000, decimal_places=2)
+    price = models.IntegerField()
 
+    def __str__(self) -> str:
+        return self.name
 
 class Code(models.Model):
     package = models.ForeignKey(Package, on_delete=models.CASCADE)
     code  = models.CharField(max_length=200)
     sold = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.code
