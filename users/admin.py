ef3ff08a04632e250ddfad5eed13eaa203bc45f4
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-11-19 10:46:29 -0500

    update: tables display

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Order, User
+from .models import Notification, Order, User
+from django.contrib.auth.models import User as CS
+from django.contrib.auth.models import Group
 
 admin.site.register(User)
 admin.site.register(Order)
+admin.site.unregister(Group)
+admin.site.unregister(CS)
+admin.site.register(Notification)
