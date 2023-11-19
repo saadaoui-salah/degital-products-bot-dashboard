4fb3396d58ffb5edc5b7669692e40cb4f27bcecf
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-11-19 10:46:51 -0500

    add: middleware

--- core/settings.py
+++ core/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.md.HeaderCheckMiddleware'
 ]
 
 ROOT_URLCONF = 'core.urls'
