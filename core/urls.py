ccc16b4b163713369f18b4726456517bfcb5f99c
saadaoui-salah <salahsaadaoui8@gmail.com>
2024-01-23 17:21:20 -0500

    add: static files urls

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+    # ... the rest of your URLconf goes here ...
+ 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     path('user/', include('users.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
