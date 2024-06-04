007e3dc49de12c744316710a44735283cf579088
saadaoui-salah <salahsaadaoui8@gmail.com>
2024-06-04 15:31:05 -0400

    add: apis urls

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,9 @@ from .views import *
 urlpatterns = [
     path('create', create_user),
     path('details/<str:tg_id>', get_details),
-    path('balance/<str:tg_id>', get_balance)
+    path('balance/<str:tg_id>', get_balance),
+    path('new_member/<str:tg_id>', get_balance),
+    path("add-to-group/<str:user_id>", add_to_group),
+    path("update-order", update_order),
+    path("report", create_report),
 ]
